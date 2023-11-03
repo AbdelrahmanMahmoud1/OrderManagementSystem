0b130b61b4c3d89bc9ba299e573662da6192d38a
MariamAnis <67607591+MariamAnis@users.noreply.github.com>
2023-11-03 13:35:14 +0200

    Add files via upload

---
+++ inventory-service/inventory-service/src/main/java/com/headwayproject/inventoryservice/response/InventoryResponse.java
@@ -0,0 +1,9 @@
+package com.headwayproject.inventoryservice.response;
+
+import com.headwayproject.inventoryservice.entity.Product;
+
+import java.util.List;
+
+public class InventoryResponse {
+    List<Product> products;
+}
