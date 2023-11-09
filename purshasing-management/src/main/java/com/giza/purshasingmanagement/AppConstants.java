8cdaae1e704dd8bd2cc5f3b9abda13f5c75590a3
ellethykhaled <ellethykhaled2@gmail.com>
2023-11-09 04:37:40 +0200

    Removing unnecessary dependencies/constants/properties

--- purshasing-management/src/main/java/com/giza/purshasingmanagement/AppConstants.java
+++ purshasing-management/src/main/java/com/giza/purshasingmanagement/AppConstants.java
@@ -1,6 +1,5 @@
 package com.giza.purshasingmanagement;
 
 public class AppConstants {
-    public static final String TOPIC_NAME = "purchase";
     public static final String INVENTORY_BASE_URL = "http://localhost:8080/";
 }
