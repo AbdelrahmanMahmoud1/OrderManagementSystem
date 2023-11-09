b2be462c62716dab39b21a15d2d2dcea3e3baac3
MariamAnis <67607591+MariamAnis@users.noreply.github.com>
2023-11-09 11:20:15 +0200

    Delete notification-service directory

--- notification-service/src/main/java/com/programming/notificationservice/Repository/NotificationEventRepository.java
+++
@@ -1,9 +0,0 @@
-package com.programming.notificationservice.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.programming.notificationservice.OrderPlacedEvent;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NotificationEventRepository extends JpaRepository<OrderPlacedEvent, Long>{
-}
