617e0e354f3a7dfe2db8c3da600fa8861efa0c57
nouranayman2 <93311077+nouranayman2@users.noreply.github.com>
2023-11-09 02:05:19 -0800

    Add files via upload

---
+++ test/order-service/order-service/src/main/java/com/project/orderservice/controller/Ordercontroller.java
@@ -0,0 +1,51 @@
+package com.project.orderservice.controller;
+
+import com.project.orderservice.config.OrderProducer;
+import com.project.orderservice.dto.OrderRequest;
+import com.project.orderservice.event.OrderPlacedEvent;
+import com.project.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/order")
+public class Ordercontroller {
+
+    //now we need to call this order service from the controller
+    private final OrderService orderService;
+    private final OrderProducer orderProducer;
+
+    public Ordercontroller(OrderService orderService, OrderProducer orderProducer) {
+        this.orderService = orderService;
+        this.orderProducer = orderProducer;
+    }
+
+
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        try {
+            orderService.placeOrder(orderRequest);
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent();
+            orderPlacedEvent.setStatus("Pending");
+            orderPlacedEvent.setMessage("Order is pending");
+            orderProducer.sendMessage(orderPlacedEvent);
+
+
+            return "Order placed Successfully";
+        } catch (Exception e) {
+            // Log the exception for debugging
+            e.printStackTrace();
+            return "Order placement failed: " + e.getMessage();
+        }
+
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello";
+    }
+}
