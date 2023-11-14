755c0bbcbff66dda585fc66d99fa5db3141ef64a
ellethykhaled <ellethykhaled2@gmail.com>
2023-11-14 23:43:34 +0200

    Cost dto adjusted

--- purshasing-management/src/main/java/com/giza/purshasingmanagement/dto/buying/CostDTO.java
+++ purshasing-management/src/main/java/com/giza/purshasingmanagement/dto/buying/CostDTO.java
@@ -1,46 +1,22 @@
 package com.giza.purshasingmanagement.dto.buying;
 
 import com.giza.purshasingmanagement.entity.buying.ProductCost;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.io.Serializable;
 
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 public class CostDTO implements Serializable {
 
     private String productName;
     private long purchaseCount;
-    private long cost;
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
-    public long getPurchaseCount() {
-        return purchaseCount;
-    }
-
-    public double increasePurchase(long purchaseCount, double price) {
-        this.purchaseCount += purchaseCount;
-        double cost = purchaseCount * price;
-        this.cost += cost;
-        return cost;
-    }
-
-    public long getCost() {
-        return cost;
-    }
-
-    @Override
-    public String toString() {
-        return "Purchase{" +
-                "productName=" + productName +
-                ", purchaseCount=" + purchaseCount +
-                ", cost=" + cost +
-                '}';
-    }
+    private double cost;
 
     static public CostDTO entityToDTO(ProductCost entity) {
         CostDTO dto = new CostDTO();
