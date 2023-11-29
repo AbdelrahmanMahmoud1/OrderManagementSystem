380a751e987f45fe8162e5dac30b24379e3e758f
ellethykhaled <ellethykhaled2@gmail.com>
2023-11-29 03:49:44 +0200

    Adjusted unit tests

--- purshasing-management/src/test/java/com/giza/purshasingmanagement/ApplicationTests.java
+++ purshasing-management/src/test/java/com/giza/purshasingmanagement/ApplicationTests.java
@@ -6,6 +6,7 @@ import com.giza.purshasingmanagement.controller.buying.response.InventoryStockRe
 import com.giza.purshasingmanagement.controller.buying.response.PurchaseDetailsResponse;
 import com.giza.purshasingmanagement.controller.selling.SellingController;
 import com.giza.purshasingmanagement.controller.selling.response.SellItemsResponse;
+import com.giza.purshasingmanagement.dto.buying.BuyingProductDTO;
 import com.giza.purshasingmanagement.dto.buying.BuyingPurchaseDTO;
 import com.giza.purshasingmanagement.dto.buying.OrderDTO;
 import com.giza.purshasingmanagement.dto.selling.SellingProductDTO;
@@ -14,10 +15,12 @@ import com.giza.purshasingmanagement.entity.Product;
 import com.giza.purshasingmanagement.service.buying.BuyingService;
 import com.giza.purshasingmanagement.service.selling.SellingService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
@@ -44,8 +47,8 @@ class ApplicationTests {
     BuyingService buyingService;
     @MockBean
     SellingService sellingService;
-    @MockBean
-    private RestTemplate restTemplate;
+    @Mock
+    private final RestTemplate restTemplate = new RestTemplate();
 
     @Test
     void submitOrderInventoryErrorTest() {
@@ -108,7 +111,7 @@ class ApplicationTests {
         when(buyingService.getPurchaseDetails()).thenReturn(expectedResponse);
 
         ResponseEntity<PurchaseDetailsResponse> actualResponse = buyingController.getPurchaseDetails();
-        assertEquals(HttpStatus.FOUND, actualResponse.getStatusCode());
+        assertEquals(HttpStatus.OK, actualResponse.getStatusCode());
         if (actualResponse.getBody() != null) {
             assertEquals(expectedResponse.getPurchaseCount(), actualResponse.getBody().getPurchaseCount());
             assertEquals(expectedResponse.getTotalCost(), actualResponse.getBody().getTotalCost());
@@ -121,9 +124,9 @@ class ApplicationTests {
         List<BuyingPurchaseDTO> purchaseDTOList = new ArrayList<>();
 
         BuyingPurchaseDTO purchase = new BuyingPurchaseDTO();
-        List<Product> products = new ArrayList<>();
-        products.add(new Product("Milk", 5, 20.0f));
-        products.add(new Product("Meat", 10, 100.0f));
+        List<BuyingProductDTO> products = new ArrayList<>();
+        products.add(new BuyingProductDTO("Milk", 5, 20.0f));
+        products.add(new BuyingProductDTO("Meat", 10, 100.0f));
         purchase.setProducts(products);
 
         purchaseDTOList.add(purchase);
@@ -134,7 +137,7 @@ class ApplicationTests {
         when(buyingService.getPurchaseDetails()).thenReturn(expectedResponse);
 
         ResponseEntity<PurchaseDetailsResponse> actualResponse = buyingController.getPurchaseDetails();
-        assertEquals(HttpStatus.FOUND, actualResponse.getStatusCode());
+        assertEquals(HttpStatus.OK, actualResponse.getStatusCode());
         if (actualResponse.getBody() != null) {
             assertEquals(expectedResponse.getPurchaseCount(), actualResponse.getBody().getPurchaseCount());
             assertEquals(expectedResponse.getTotalCost(), actualResponse.getBody().getTotalCost());
@@ -159,7 +162,7 @@ class ApplicationTests {
         when(sellingService.getPurchaseDetails()).thenReturn(expectedResponse);
 
         ResponseEntity<com.giza.purshasingmanagement.controller.selling.response.PurchaseDetailsResponse> actualResponse = sellingController.getPurchaseDetails();
-        assertEquals(HttpStatus.FOUND, actualResponse.getStatusCode());
+        assertEquals(HttpStatus.OK, actualResponse.getStatusCode());
         if (actualResponse.getBody() != null) {
             assertEquals(expectedResponse.getPurchaseCount(), actualResponse.getBody().getPurchaseCount());
             assertEquals(expectedResponse.getTotalRevenue(), actualResponse.getBody().getTotalRevenue());
@@ -185,7 +188,7 @@ class ApplicationTests {
         when(sellingService.getPurchaseDetails()).thenReturn(expectedResponse);
 
         ResponseEntity<com.giza.purshasingmanagement.controller.selling.response.PurchaseDetailsResponse> actualResponse = sellingController.getPurchaseDetails();
-        assertEquals(HttpStatus.FOUND, actualResponse.getStatusCode());
+        assertEquals(HttpStatus.OK, actualResponse.getStatusCode());
         if (actualResponse.getBody() != null) {
             assertEquals(expectedResponse.getPurchaseCount(), actualResponse.getBody().getPurchaseCount());
             assertEquals(expectedResponse.getTotalRevenue(), actualResponse.getBody().getTotalRevenue());
